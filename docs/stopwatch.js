14abbe954e86c8c6e59759d1612b634c93304ee5
naga <13200956+nagasawaaaa@users.noreply.github.com>
2017-11-15 17:48:50 +0900

    Add files via upload

---
+++ docs/stopwatch.js
@@ -0,0 +1,80 @@
+new Vue({
+  el: "#app",
+  data: {
+    times: [],
+    animateFrame: 0,
+    nowTime: 0,
+    diffTime: 0,
+    startTime: 0,
+    isRunning: false
+  },
+  methods: {
+    // 現在時刻から引数に渡した数値を startTime に代入
+    setSubtractStartTime: function (time) {
+      var time = time === undefined ? 0 : time;
+      this.startTime = Math.floor(performance.now() - time);
+    },
+    // タイマーをスタートさせる
+    startTimer: function () {
+      // loop()内で this の値が変更されるので退避
+      var vm = this;
+      vm.setSubtractStartTime(vm.diffTime);
+      // ループ処理
+      (function loop(){
+        vm.nowTime = Math.floor(performance.now());
+        vm.diffTime = vm.nowTime - vm.startTime;
+        vm.animateFrame = requestAnimationFrame(loop);
+      }());
+      vm.isRunning = true;
+    },
+    // タイマーを停止させる
+    stopTimer: function () {
+      this.isRunning = false;
+      cancelAnimationFrame(this.animateFrame);
+    },
+    // 計測中の時間を配列に追加
+    pushTime: function () {
+      this.times.push({
+        hours: this.hours,
+        minutes: this.minutes,
+        seconds: this.seconds,
+        milliSeconds: this.milliSeconds
+      });
+    },
+    // 初期化
+    clearAll: function () {
+      this.startTime = 0;
+      this.nowTime = 0;
+      this.diffTime = 0;
+      this.times = [];
+      this.stopTimer();
+      this.animateFrame = 0;
+    }
+  },
+  computed: {
+    // 時間を計算
+    hours: function () {
+      return Math.floor(this.diffTime / 1000 / 60 / 60);
+    },
+    // 分数を計算 (60分になったら0分に戻る)
+    minutes: function () {
+      return Math.floor(this.diffTime / 1000 / 60) % 60;
+    },
+    // 秒数を計算 (60秒になったら0秒に戻る)
+    seconds: function () {
+      return Math.floor(this.diffTime / 1000) % 60;
+    },
+    // ミリ数を計算 (1000ミリ秒になったら0ミリ秒に戻る)
+    milliSeconds: function () {
+      return Math.floor(this.diffTime % 1000);
+    }
+  },
+  filters: {
+    // ゼロ埋めフィルタ 引数に桁数を入力する
+    // ※ String.prototype.padStart() は IEじゃ使えない
+    zeroPad: function(value, num){
+      var num = num === undefined ? 2 : num;
+      return value.toString().padStart(num,"0");
+    }
+  }
+});
